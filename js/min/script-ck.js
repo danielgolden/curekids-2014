3f03e6a58f04f0cb5d6d9aca27a178e15d1bc703
danielgolden <danielgolden90@gmail.com>
2014-07-15 13:56:20 -0400

    Deleted several unused files and removed config.codekit from git

--- js/min/script-ck.js
+++
@@ -1,1 +0,0 @@
-$(function(){$("div[data-liffect] .ck-profile-photo-thumb").each(function(a){$(this).attr("style","-webkit-animation-delay:"+35*a+"ms;-moz-animation-delay:"+35*a+"ms;-o-animation-delay:"+35*a+"ms;animation-delay:"+35*a+"ms;"),a==$("div[data-liffect] .ck-profile-photo-thumb").size()-1&&$("div[data-liffect]").addClass("play")})});
