d9725a6c2bf43f7e666b2b45db43513b4dc65c77
danielgolden <danielgolden90@gmail.com>
2014-07-18 13:58:22 -0400

    Finishes responsive styling - finishes responsive styling of footer - Adds script and styles for larger CUREkid names below 370px viewport width - a few other minor changes - All that's left, concerning styling, is a few bugs to fix

--- js/script.js
+++ js/script.js
@@ -16,4 +16,14 @@ $(function() {
 			$('.primary-header').addClass('mobile-nav-active');
 		}
 	});
+
+	$('.get-well-options-toggle').click(function(e) {
+		e.preventDefault();
+		$('.get-well-options').addClass('active');
+		$(this).hide();
+	});
+
+	if($('.ck-profile-name').width() > 190) {
+		$('.ck-profile-header-col-1').addClass('large-name');
+	}
 });
