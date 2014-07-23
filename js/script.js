ac4f6b88ced1ff889bfd2c34df6d138fb76ff627
danielgolden <danielgolden90@gmail.com>
2014-07-23 16:44:13 -0400

    Adds "view more photos" button - This button is hidden by default and visible on hover of the photos container if the device is not touch enabled (via Modernizr) - If touch is enabled then the button is always visible

--- js/script.js
+++ js/script.js
@@ -1,14 +1,4 @@
 $(function() {
-	$("div[data-liffect] .ck-profile-photo-thumb").each(function (i) {
-	    $(this).attr("style", "-webkit-animation-delay:" + i * 35 + "ms;"
-	            + "-moz-animation-delay:" + i * 35 + "ms;"
-	            + "-o-animation-delay:" + i * 35 + "ms;"
-	            + "animation-delay:" + i * 35 + "ms;");
-	    if (i == $("div[data-liffect] .ck-profile-photo-thumb").size() -1) {
-	        $("div[data-liffect]").addClass("play");
-	    }
-	});
-
 	$('.menu-toggle').click(function() {
 		if($('.primary-header').hasClass('mobile-nav-active')) {
 			$('.primary-header').removeClass('mobile-nav-active');
